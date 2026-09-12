94b9c96670babd824ebc8fc07b3f70b18865fb2b
Tselot <amaretselot@gmail.com>
2026-09-12 10:08:52 -0700

    Implement cycle detection in linked list

---
+++ Linked List Cycle.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True 
+        else: 
+            return False
